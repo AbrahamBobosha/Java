c5728d47a2333bccfd280428c5938aad0bbed4b0
Abraham Bobosha <abtho16@student.sdu.dk>
2021-03-09 19:03:04 +0100

    Add files via upload

---
+++ Interface/InterfaceExample.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Interface;
+
+/**
+ *
+ * @author abli
+ */
+class Phone 
+{
+    public void call() { System.out.println("Phone call"); }
+    public void sms() { System.out.println("Phone sending SMS"); }
+}
+
+interface ICamera
+{
+    void click();
+    void record();
+}
+
+interface IMusicPlayer
+{
+    void play();
+    void stop();
+}
+
+class SmartPhone extends Phone implements ICamera,IMusicPlayer
+{
+    public void videoCall() { System.out.println("Smart Phone video calling"); }
+   
+    public void click() { System.out.println("Smart Phone Clicking Photo"); }
+    public void record() { System.out.println("Smart Phone recording video"); }
+    public void play() { System.out.println("Smart Phone playing music"); }
+    public void stop() { System.out.println("Smart Phone stopped playing music"); }
+
+}
+public class InterfaceExample 
+{
+
+    public static void main(String[] args) 
+    {
+        IMusicPlayer sp=new SmartPhone();
+        sp.play();
+        sp.stop();
+        
+    }
+    
+}
